be10118363835dcd0fbeb5f2c318d25acafadab6
Connor Redmond <connred@icloud.com>
2017-11-08 00:38:11 -0600

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,33 @@
 import java.util.*;
+import java.lang.*;
 public class Main {
 	private static Scanner in;
 	public static void main(String[] args) {
-		int iInt = 1;
-		int moveCount = 0;
+		int iInt = 0;
+		int moveCount = 1;
 		int eInt = 0;
-		int currentLargest = 0;
-		int editedLargest = 0;
-		boolean cEven = true;
-		getInt(iInt);
-		if (checkForZeros(iInt) == true) {
-			isZeros(iInt, eInt, moveCount, currentLargest, cEven, editedLargest);
-		} else {
-			//noZeros(iInt, eInt, moveCount, currentLargest, cEven, editedLargest);
+		int len = 0;
+		iInt = getInt(iInt);
+		if (iInt < 0) {
+			iInt *= -1;
 		}
+		while (iInt != 0) {
+			len = (int)(Math.log10(iInt));
+			boolean choice = checkForZeros(iInt, len);
+			if (choice == true) {
+				iInt = isZeros(iInt, eInt,len);
+				System.out.println(iInt);
+				if (iInt > 0) {moveCount +=1;}
+			} else {
+				iInt = noZeros(iInt, eInt,len);
+				System.out.println(iInt);
+				moveCount +=1;
+			}
+		}
+		System.out.println(moveCount);
 	}
+		
+
 	///////////////////////
 	// UNIVERSAL METHODS //
 	///////////////////////
@@ -25,77 +38,131 @@ public class Main {
 		iInt = in.nextInt();
 		return iInt;
 	}
-	public static boolean checkForZeros(int iInt) {
+	public static boolean checkForZeros(int iInt,int len) {
 		// Checks the int to see is there are any Zeros
-		 while(iInt > 0) {
-		        if(iInt % 10 == 0)
-		            return true;
-		        iInt /= 10;
-		    }
-		 return false;	
-	}
-	private static boolean checkType(int currentLargest, boolean cEven) {
-		if (currentLargest%2 == 0 ) {
-			cEven = true;
-		} else {
-			cEven = false;
+		List<Integer> number = new ArrayList<Integer>();
+		int x = 10;
+		int y = 1;
+		int z = 0;
+		int zero = 0;
+		for (int i = 0; i < len; i++) {
+			z = (iInt % x) / y;
+			x *= 10;
+			y *= 10;
+			number.add(z);
 		}
-		return cEven;
+		zero = number.indexOf(0);
+		if (zero > -1) {
+			return true;
+		} else {
+			return false;
+		}
+		 
 	}
-	private static int findLargest(int eInt, int currentLargest) {
-		// TODO Find the largest Value in the integer
-		// set that number = currentLargest and return it
-		
-		//last
-		return currentLargest;
-	}
-
 	//////////////
 	// is Zeros //
 	//////////////
-	private static void isZeros(int iInt, int eInt, int moveCount, int currentLargest, boolean cEven, int editedLargest) {
-			// TODO Functions that run when a Zero is found
-		findAndRemove(iInt, eInt);
-		findLargest(eInt, currentLargest);
-		checkType(currentLargest, cEven);
-		if(cEven == true) {
-			editedLargest = currentLargest - 2;
-		} else {
-			editedLargest = currentLargest - 1;
-		}
-		findAndRepalce(eInt, currentLargest, editedLargest);	
+	private static int isZeros(int iInt, int eInt, int len) {
 		eInt = iInt;
-		moveCount += 1;
+		iInt = findAndRemove(iInt, eInt, len);
+		return iInt;
 	}
-	private static int findAndRepalce(int eInt, int currentLargest, int editedLargest) {
-		// TODO Auto-generated method stub
-		// Search for currentLargest in eInt and replace it with editedLargest.
-		// int tInt = 0; //use a temp int to manipulate then set it equal to eInt once you have the final edited value
+	private static int findAndRemove(int iInt, int eInt, int len) {
+		List<Integer> number = new ArrayList<Integer>();
+		int x = 10;
+		int y = 1;
+		int z = 0;
+		int zero = 0;
+		int tlen = 0;
+		for (int i = 0; i < len; i++) {
+			z = (iInt % x) / y;
+			x *= 10;
+			y *= 10;
+			number.add(z);
+		}
+		zero = number.indexOf(0);
+		for (int i = zero; i < len;i++) {
+				number.remove(zero);
+			}
+		if (zero == 0)
+			number.removeAll(number);
+		tlen = number.size();
+		int p = 1;
+		eInt = 0;
+		if (zero > 0) {
+			for (int i = 0; i < tlen;i++) {
+				int k = number.get(i);
+				eInt = eInt + (k * p);
+				p *= 10;
+			}
+		} else {
+			eInt = 0;
+		}
 		
-		
-		
-		
-		return eInt;
-	}
-	private static int findAndRemove(int iInt, int eInt) {
-		// TODO Find Zeros and Remove #s to the left
-
-		// First find the zero AND WHERE IT IS reading from right to left
-		// Then remove all numbers to the left
-		// assign the new number to eInt
-		//this is last
 		return eInt;
 	}
 	//////////////
 	// NO ZEROS //
 	//////////////
-	/*private static int noZeros(int iInt) {
-		// TODO Functions that run when no zeros are found
-		findLargest(iInt);
-		checkType(iInt);
-		
-		//last
+	private static int noZeros(int iInt, int eInt, int len) {
+		eInt = iInt;
+		iInt = findAndReplace(eInt);
 		return iInt;
+	}
+	private static int findAndReplace(int eInt) {
+		// Search for currentLargest in eInt and replace it with editedLargest.
+		// int tInt = 0; //use a temp int to manipulate then set it equal to eInt once you have the final edited value
+		int len = (int)(Math.log10(eInt)+1);
+		int l = findL(eInt);
+		int n = 0;
+		boolean type = evenOrOdd(l); // true = even / false = odd
+		if (type == true) {
+			n = l - 2;
+		}
+		if (type == false) {
+			n = l - 1;
+		}
+		List<Integer> number = new ArrayList<Integer>();
+		int z = 0;
+		int x = 10;
+		int y = 1; 
+		int tlen = 0;
+		for (int i = 0; i < len; i++) {
+			z = (eInt % x) / y;
+			x *= 10;
+			y *= 10;
+			number.add(z);
+		}
+		l = number.indexOf(l);
+		number.set(l,n);
+		tlen = number.size();
+		eInt = 0;
+		int p = 1;
+		for (int i = 0; i < tlen;i++) {
+			int k = number.get(i);
+			eInt = eInt + (k * p);
+			p *= 10;
+		}
 		
-	}*/
+		return eInt;
+	}
+	private static boolean evenOrOdd(int l) {
+		if (l%2 == 0 ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+	private static int findL(int eInt) {
+		int largest=0; 
+		while( eInt > 0 ) { 
+			int digit = eInt % 10; 
+			eInt = eInt / 10; 
+			if (digit > largest) largest = digit; 
+		}
+		return largest;
+	}
+
 }
